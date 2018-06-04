6e8a8435a601f2bb615a1c152c0d65a845526d91
dabanli123 <1031737848@qq.com>
2018-06-04 15:44:34 +0800

    竞拍功能子页面

--- src/pages/bid/auctioninfo.vue
+++ src/pages/bid/auctioninfo.vue
@@ -18,7 +18,7 @@
           </div>
           <div class="form-box">
               <div class="neoname"> BunnyRepublic.neo</div>
-              <div class="tips">Tips : When you successfully get your domain,You can edit it in My Neo Name.</div>
+              <div class="neoname-tips">Tips : When you successfully get your domain,You can edit it in My Neo Name.</div>
               <div class="btn-center">
                 <button class="btn btn-nel btn-bid" >Get domain</button>
                 <button class="btn btn-nel btn-bid btn-disable" disabled>Getting domain...</button>
@@ -63,7 +63,109 @@
             <span>Timeline</span>
         </div>
         <div class="form-box">
-            <div class="time-box">sdfasf</div>
+          <!-- process-wrapper start -->
+          <div class="process-wrapper">
+            <label>Status</label>
+            <div class="process-box">
+              <div class="process long" style="width:70%">
+                <div class="process-tips">Random period </div>
+                <!-- <div class="process-fff">
+                  <img src="../../../static/img/going.png" alt="">
+                </div> -->
+              </div>
+              <div class="starts">2018/05/24
+                <br/>16:00:00</div>
+
+              <div class="days">
+                <em>Bid start time</em>
+                <span>2018/05/24
+                  <br/>16:00:00</span>
+              </div>
+              <div class="days">
+                <span>2018/05/24
+                  <br/>16:00:00</span>
+              </div>
+              <div class="days">
+                <em>Fixed period end time</em>
+                <span>2018/05/24
+                  <br/>16:00:00</span>
+              </div>
+              <div class="days">
+                <span>2018/05/24
+                  <br/>16:00:00</span>
+              </div>
+              <div class="days">
+                <em>Maximum end time of random period</em>
+                <span>2018/05/24
+                  <br/>16:00:00</span>
+              </div>
+            </div>
+          </div>
+          <!-- process-wrapper end  -->
+          <div class="auction-tips">Tips : The auction is about going to random period, and it may end at any point during the random period.</div>
+          <!-- timeling-wrapper start -->
+          <div class="timeling-wrapper">
+            <div class="first"></div>
+            <div class="list">
+              <div class="line"></div>
+              <div class="infos">
+                <span>2018/05/24 10:30:21</span>
+                <p>Other people（AYMa...tquh）</p>
+                <em>Price：9 SGas</em>
+              </div>
+            </div>
+
+            <div class="list">
+              <div class="line"></div>
+              <div class="infos">
+                <span>2018/05/24 10:30:21</span>
+                <p>Other people（AYMa...tquh）</p>
+                <em>Price：9 SGas</em>
+              </div>
+            </div>
+
+            <div class="list">
+              <div class="line"></div>
+              <div class="infos">
+                <span>2018/05/24 10:30:21</span>
+                <p>Other people（AYMa...tquh）</p>
+                <em>Price：9 SGas</em>
+              </div>
+            </div>
+
+            <div class="list">
+              <div class="line"></div>
+              <div class="infos">
+                <span>2018/05/24 10:30:21</span>
+                <p>Other people（AYMa...tquh）</p>
+                <em>Price：9 SGas</em>
+              </div>
+            </div>
+
+
+            <div class="list">
+              <div class="line"></div>
+              <div class="infos">
+                <span>2018/05/24 10:30:21</span>
+                <p>Other people（AYMa...tquh）</p>
+                <em>Price：9 SGas</em>
+              </div>
+            </div>
+
+
+            <div class="list">
+              <div class="line"></div>
+              <div class="infos">
+                <span>2018/05/24 10:30:21</span>
+                <p>Other people（AYMa...tquh）</p>
+                <em>Price：9 SGas</em>
+              </div>
+            </div>
+          </div>
+          <!-- timeling-wrapper end -->
+          <div class="viewmore">
+            <button class="btn btn-nel">View more</button>
+          </div>
         </div>
     </div>
 </template>
@@ -154,12 +256,12 @@
     }
     .my-sgas {
       margin-top: 20px;
-      margin-bottom: 50px;
+      margin-bottom: 30px;
     }
     .tips-msg {
       font-size: 14px;
       color: #c5c5c5;
-      margin-bottom: 30px;
+      margin-bottom: 50px;
     }
     .btn-bid-box {
       text-align: center;
@@ -174,7 +276,7 @@
       text-align: center;
       font-size: 24px;
     }
-    .tips {
+    .neoname-tips {
       margin-top: 20px;
       margin-bottom: 30px;
       text-align: center;
@@ -195,6 +297,237 @@
     }
     .remain-msg {
       margin-bottom: 50px;
+    }
+    .auction-tips {
+      font-size: 14px;
+      color: #c5c5c5;
+      text-align: left;
+      margin-top: 30px;
+      margin-left: 34px;
+    }
+    // process-wrapper
+    .process-wrapper {
+      padding-top: 26px;
+      margin-left: 34px;
+      height: 88px;
+      display: inline-block;
+      position: relative;
+      label {
+        font-size: 16px;
+        color: #ffffff;
+        line-height: 16px;
+        vertical-align: middle;
+        margin-right: 15px;
+      }
+      .process-box {
+        display: inline-block;
+        vertical-align: middle;
+        height: 20px;
+        background: #404754;
+        box-shadow: inset 0 3px 3px 0 rgba(0, 0, 0, 0.5);
+        border-radius: 3px;
+        position: relative;
+
+        .days {
+          width: 150px;
+          height: 100%;
+          border-right: 1px solid rgba(255, 255, 255, 0.5);
+          display: inline-block;
+          position: relative;
+
+          font-size: 12px;
+          color: #ffffff;
+          line-height: 14px;
+
+          &:last-child {
+            border: none;
+          }
+
+          em {
+            font-style: normal;
+            position: absolute;
+            top: -26px;
+            white-space: nowrap;
+            right: -70px;
+          }
+
+          span {
+            position: absolute;
+            top: 35px;
+            right: -34px;
+          }
+        }
+
+        div:nth-of-type(3) {
+          em {
+            left: -35px;
+          }
+        }
+
+        .starts {
+          font-size: 12px;
+          color: #ffffff;
+          line-height: 14px;
+
+          position: absolute;
+          top: 35px;
+          left: -34px;
+        }
+      }
+
+      .process {
+        height: 100%;
+        position: absolute;
+        left: 0;
+        top: 0;
+        background: #2dde4f;
+        //background-image: linear-gradient(90deg, #2dde4f 0%, #2dde4f 100%);
+        box-shadow: inset 0 3px 3px 0 rgba(255, 255, 255, 0.3);
+        border-radius: 3px;
+        z-index: 1;
+
+        &.long {
+          background-image: linear-gradient(90deg, #2dde4f 0%, #dfa757 100%);
+          box-shadow: inset 0 3px 3px 0 rgba(255, 255, 255, 0.3);
+        }
+        &:hover {
+          .process-tips {
+            display: block;
+          }
+        }
+
+        .process-tips {
+          display: none;
+          padding: 9px 5px;
+          height: 30px;
+          box-sizing: border-box;
+          font-size: 12px;
+          color: #fff;
+          white-space: nowrap;
+          background: #151a1e;
+          border-radius: 5px;
+          position: absolute;
+          top: -40px;
+          right: -46px;
+          line-height: 12px;
+          &:before {
+            content: "";
+            display: block;
+            width: 0;
+            height: 0;
+            border: 5px solid transparent;
+            border-top-color: #151a1e;
+            position: absolute;
+            bottom: -10px;
+            left: 50%;
+            margin-left: -5px;
+          }
+        }
+        .process-fff {
+          width: 20px;
+          height: 20px;
+          position: absolute;
+          right: -2px;
+          top: 1px;
+          img {
+            width: 100%;
+          }
+        }
+      }
+    }
+    // timeling-wrapper
+    .timeling-wrapper {
+      overflow: hidden;
+      height: auto;
+      margin-top: 100px;
+      .first {
+        width: 1px;
+        height: 100px;
+        background: #fff;
+        margin: 0 auto;
+      }
+
+      .list {
+        margin: 0 auto;
+        width: 1px;
+        position: relative;
+        .line {
+          height: 100px;
+          width: 1px;
+          background: #fff;
+          position: relative;
+
+          &:after {
+            content: "";
+            display: block;
+            width: 11px;
+            height: 11px;
+            border-radius: 50%;
+            background: #198cee;
+            position: absolute;
+            left: -5px;
+          }
+        }
+
+        .infos {
+          width: 305px;
+          background: #151a1e;
+          color: #fff;
+          padding: 15px 20px;
+          position: absolute;
+          left: -331px;
+          top: -50%;
+          margin-top: 10px;
+          box-sizing: border-box;
+
+          &:after {
+            width: 0;
+            height: 0;
+            border: 14px solid transparent;
+            border-left-color: #151a1e;
+            position: absolute;
+            content: "";
+            display: block;
+            right: -28px;
+            top: 50%;
+            margin-top: -14px;
+          }
+
+          span {
+            font-size: 12px;
+            color: #ffffff;
+          }
+
+          p {
+            font-size: 14px;
+            color: #ffffff;
+          }
+
+          em {
+            font-style: normal;
+            font-size: 14px;
+            color: #ffffff;
+          }
+        }
+
+        &:nth-of-type(2n-1) {
+          .infos {
+            right: -331px;
+            left: initial;
+
+            &:after {
+              border: 14px solid transparent;
+              border-right-color: #151a1e;
+              left: -28px;
+              right: initial;
+            }
+          }
+        }
+      }
+    }
+    .viewmore {
+      text-align: center;
+      margin-top: 50px;
     }
   }
 }
