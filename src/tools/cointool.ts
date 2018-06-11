0472fb8bae1acbaaedeee96d78caecf98a8c1a13
vincentOpenSource <13916486874@163.com>
2018-06-11 15:30:21 +0800

    合约地址和转账地址

--- src/tools/cointool.ts
+++ src/tools/cointool.ts
@@ -5,7 +5,7 @@ export class CoinTool
 {
     static readonly id_GAS: string = "0x602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7";
     static readonly id_NEO: string = "0xc56f33fc6ecfcd0c225c4ab356fee59390af8560be0e930faebe74a6daff7c9b";
-    static readonly id_SGAS: Neo.Uint160 = Neo.Uint160.parse('e52a08c20986332ad8dccf9ded38cc493878064a');
+    static readonly id_SGAS: Neo.Uint160 = Neo.Uint160.parse('c7816d11287c08135f4e5f907af9e39754910ba3');
     static assetID2name: { [ id: string ]: string } = {};
     static name2assetID: { [ id: string ]: string } = {};
 
