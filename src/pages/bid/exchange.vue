b1e163f66c11b094d5b797890426f6049df01949
dabanli123 <1031737848@qq.com>
2018-06-12 18:47:50 +0800

    修改exchange模块的整体显示，以及修改转账效果显示

--- src/pages/bid/exchange.vue
+++ src/pages/bid/exchange.vue
@@ -5,55 +5,97 @@
         </div>
         <div class="form-box">
             <div class="exchange-sgas">
-                <div class="choose-wrap choose-left" :class="{'active':!changeSGas}" @click="exchangeTranType(false)">
-                    <img src="../../../static/img/sgas-nomal.png" alt="sgas-nomal.png" v-if="!changeSGas">
-                    <img src="../../../static/img/sgas-gray.png" alt="sgas-gray.png" v-if="changeSGas">
-                    <span>Exchange Gas for SGas</span>
-                </div>
-                <div class="choose-wrap choose-right" :class="{'active':changeSGas}" @click="exchangeTranType(true)">
-                    <img src="../../../static/img/gas-nomal.png" alt="gas-nomal.png" v-if="changeSGas">
-                    <img src="../../../static/img/gas-gray.png" alt="gas-gray.png" v-if="!changeSGas">
-                    <span>Exchange SGas for Gas</span>
-                </div>
-            </div>
-            <div class="trans-box">
-                <div class="spent-box">
-                    <div class="spent-msg">
-                        Amount you will spent : 
-                    </div>
-                   <div class="spent-input">
-                        <input type="number" placeholder="0" v-model="transcount" @input="exchangeCount">
-                        <span>{{changeSGas?"SGas":"Gas"}}</span>
-                    </div>
-                    <div class="spent-tip">
-                       It will cost you <span>{{transcount?transcount:0}}</span> Gas. ( you currently have <span :class="{'ff6' : transcount > exMaxcount}">{{changeSGas?mySGas:myGas}}</span>  Gas. )
+              <div class="gas-sgas" v-if="!changeSGas">
+                <div class="choose-wrap wrap-left">
+                    <img src="../../../static/img/gas-nomal.png" alt="gas-nomal.png">
+                    <div class="change-type">
+                      <span>Gas</span>
+                      <p>Balance: <span>{{myGas}}</span></p>
                     </div>
                 </div>
                 <div class="guid-img">
                     <img src="../../../static/img/guiding.png" alt="guiding.png">
                 </div>
-                <div class="receive-box">
-                    <div class="receive-msg">
-                        Amount you will receive :  
-                    </div>
-                    <div class="receive-input">
-                        <input type="number" placeholder="Amount" v-model="transcount" disabled>
-                        <span>{{changeSGas?"Gas":"SGas"}}</span>
-                    </div>
-                    <div class="receive-tip">
-                       You will have <span>{{excount}}</span> SGas. ( You currently have <span>{{changeSGas?myGas:mySGas}}</span> SGas. )
+                <div class="choose-wrap wrap-right">
+                    <img src="../../../static/img/sgas-nomal.png" alt="sgas-nomal.png">
+                    <div class="change-type">
+                      <span>SGas</span>
+                      <p>Balance: <span>{{mySGas}}</span></p>
                     </div>
                 </div>
+              </div>
+              <div class="gas-sgas" v-if="changeSGas">
+                <div class="choose-wrap wrap-left">
+                    <img src="../../../static/img/sgas-nomal.png" alt="sgas-nomal.png" >
+                    <div class="change-type">
+                      <span>SGas</span>
+                      <p>Balance: <span>{{mySGas}}</span></p>
+                    </div>
+                </div>
+                <div class="guid-img">
+                    <img src="../../../static/img/guiding.png" alt="guiding.png">
+                </div>
+                <div class="choose-wrap wrap-right">
+                    <img src="../../../static/img/gas-nomal.png" alt="gas-nomal.png">
+                    <div class="change-type">
+                      <span>Gas</span>
+                      <p>Balance: <span>{{myGas}}</span></p>
+                    </div>
+                </div>
+              </div>
+              <div class="exchange-img" @click="exchangeTranType">
+                  <img src="../../../static/img/exchange.png" alt="exchange.png">
+              </div>
             </div>
+            <div class="sgas-tip">
+              Tips：SGAS is a NEP5 token，which is bound with NEO’s GAS at the ratio of 1:1 and they can be converted with each other freely.
+            </div>
+            <div class="trans-box">
+                <div class="spent-box">
+                  <div class="spent-msg">
+                    Amount you will spent : 
+                  </div>
+                  <div class="spent-input">
+                    <div class="img-icon">
+                      <img src="../../../static/img/plus.png" alt="plus.png">
+                    </div>
+                    <div class="input-icon">
+                      <input type="number" placeholder="0" v-model="transcount" @input="exchangeCount">
+                    </div>
+                    <div class="msg-icon">
+                      {{changeSGas?"SGas":"Gas"}}
+                    </div>
+                  </div>
+                </div>
+                <div class="spent-box">
+                  <div class="spent-msg">
+                    Amount you will receive :  
+                  </div>
+                  <div class="spent-input disable-input">
+                    <div class="img-icon">
+                      <img src="../../../static/img/minus.png" alt="minus.png">
+                    </div>
+                    <div class="input-icon">
+                      <input type="number" placeholder="Amount" v-model="transcount" disabled>
+                    </div>
+                    <div class="msg-icon">
+                      {{changeSGas?"Gas":"SGas"}}
+                    </div>
+                  </div>
+                </div>
+                <div class="spent-tip ff6" v-if="transcount > exMaxcount">
+                  Insufficient balance. 
+              </div>
+            </div>            
             <div class="btn-right">
-                <button class="btn btn-nel btn-big" @click="exChange()" :class="{'btn-disable':(transcount > exMaxcount && !exchangebtn)}" :disabled="transcount > exMaxcount && !exchangebtn">Exchange</button>
+                <button class="btn btn-nel btn-big" @click="exChange()" :class="{'btn-disable':(transcount > exMaxcount || !exchangebtn)}" :disabled="transcount > exMaxcount || !exchangebtn">Exchange</button>
             </div>
         </div>
-        <div class="form-box tran-list">
+        <div class="form-box tran-list" v-if="exchangeList">
           <h3 class="tran-title">Waiting for transaction records</h3>
-          <div class="tran-history" id="addtranlist">            
-            <p>Exchange Gas for SGas：1 Gas, TXID: 0x8f7b1cbd2c0bd7757895f8353d02714ad693cba841e706d8e4cbef9207e142fc</p>
-            <p>Exchange Gas for SGas：1 Gas, TXID: 0x8f7b1cbd2c0bd7757895f8353d02714ad693cba841e706d8e4cbef9207e142fc</p>
+          <div class="tran-history">            
+            <p >Exchange Gas for SGas：{{exchangeList.trancount}} {{exchangeList.trantype}}, TXID: {{exchangeList.txid}}</p>
+            <p v-if="exchangeList.trantype == 'SGas'">Exchange SGas for Gas：{{exchangeList.trancount}} {{exchangeList.trantype}}, TXID: {{exchangeList.txid}}</p>
           </div>
         </div>
     </div>
@@ -80,128 +122,127 @@
     font-size: 16px;
     margin-bottom: 20px;
     .exchange-sgas {
-      width: 600px;
       display: flex;
-      .choose-wrap {
-        flex: 1;
-        height: 50px;
-        text-align: center;
-        line-height: 50px;
+      align-items: center;
+      .gas-sgas {
         border: 1px solid #c5c5c5;
-        &.choose-left {
-          border-radius: 5px 0 0 5px;
-          border-right: none;
+        border-radius: 5px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 70%;
+        height: 75px;
+        margin-right: 20px;
+        .choose-wrap {
+          width: 35%;
+          display: flex;
+          align-items: center;
+          .change-type {
+            margin-left: 10px;
+            font-size: 16px;
+            color: #fff;
+            line-height: 16px;
+            p {
+              font-size: 12px;
+              color: #fff;
+              line-height: 12px;
+              margin-bottom: 0;
+              margin-top: 7px;
+            }
+          }
+          &.wrap-left {
+            margin-left: 80px;
+          }
+          &.wrap-right {
+            margin-right: 80px;
+          }
+          img {
+            width: 30px;
+            height: 30px;
+          }
         }
-        &.choose-right {
-          border-radius: 0 5px 5px 0;
-          border-left: none;
+        .guid-img {
+          display: inline-block;
+          width: 30%;
+          text-align: center;
         }
-        &.active {
-          background: #198cee;
-          border: none;
-        }
+      }
+      .guid-img,
+      .exchange-img {
         img {
           width: 30px;
           height: 30px;
-          margin-right: 15px;
         }
       }
+    }
+    .sgas-tip {
+      font-size: 12px;
+      margin-top: 10px;
     }
     .trans-box {
       margin-top: 50px;
       margin-bottom: 50px;
-      display: flex;
       .spent-box {
-        //width: 450px;
-        flex: 1;
-        position: relative;
+        display: inline-block;
+        width: 45%;
+        margin-right: 30px;
         .spent-msg {
           margin-bottom: 20px;
         }
         .spent-input {
           width: 100%;
           height: 56px;
-          position: relative;
-          background: #6d7480;
-          border: 1px solid #b2b2b2;
-          border-radius: 5px;
-          input {
-            width: 80%;
-            height: 100%;
-            background: none;
-            border: none;
-            &::-webkit-input-placeholder {
-              font-size: 16px;
-              color: #c5c5c5;
-            }
-          }
-          span {
-            font-size: 16px;
-            position: absolute;
-            top: 16px;
-            right: 20px;
-          }
-        }
-        .spent-tip {
-          font-size: 12px;
-          color: #ffffff;
-          line-height: 12px;
-          position: relative;
-          bottom: -10px;
-          left: 0;
-        }
-      }
-      .guid-img {
-        margin-left: 25px;
-        margin-right: 25px;
-        position: relative;
-        top: 55px;
-        img {
-          width: 30px;
-          height: 30px;
-        }
-      }
-      .receive-box {
-        flex: 1;
-        //width: 450px;
-        position: relative;
-        .receive-msg {
-          margin-bottom: 20px;
-        }
-        .receive-input {
-          width: 100%;
-          height: 56px;
-          position: relative;
           background: #454f60;
           border: 1px solid #b2b2b2;
           border-radius: 5px;
-          input {
-            width: 80%;
-            height: 100%;
-            background: none;
-            border: none;
-            &::-webkit-input-placeholder {
-              font-size: 16px;
-              color: #c5c5c5;
+          position: relative;
+          &.disable-input {
+            background: #6d7480;
+          }
+          .img-icon {
+            display: inline-block;
+            vertical-align: middle;
+            font-size: 24px;
+            color: #ff6966;
+            width: 15px;
+            margin-left: 20px;
+            img {
+              width: 100%;
             }
           }
-          span {
+          .input-icon {
+            display: inline-block;
+            vertical-align: middle;
+            width: 65%;
+            height: 100%;
+            input {
+              width: 100%;
+              height: 100%;
+              background: none;
+              border: none;
+              &::-webkit-input-placeholder {
+                font-size: 16px;
+                color: #c5c5c5;
+              }
+            }
+          }
+          .msg-icon {
             font-size: 16px;
+            vertical-align: middle;
             position: absolute;
-            top: 16px;
+            top: 15px;
             right: 20px;
           }
         }
-        .receive-tip {
-          font-size: 12px;
-          color: #ffffff;
-          line-height: 12px;
-          position: relative;
-          bottom: -10px;
-          left: 0;
-        }
+      }
+      .spent-tip {
+        font-size: 12px;
+        color: #ff6a6a;
+        line-height: 12px;
+        margin-top: 10px;
       }
     }
+
     .btn-right {
       text-align: right;
     }
